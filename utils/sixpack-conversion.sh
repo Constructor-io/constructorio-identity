d204582f51f126d8aea2e66f26d9115875e9f87a
Zubin Tiku <rubikzube@gmail.com>
2018-01-23 13:28:25 -0800

    Removed conversion script because it's not useful anymore

--- utils/sixpack-conversion.sh
+++
@@ -1,5 +0,0 @@
-# Conversion script to change sixpack codebase to constructor codebase
-perl -pi -e 's/sixpack/ConstructorioAB/g' sixpack.js
-perl -pi -e 's|http://localhost:5001|https://ab.cnstrc.com|g' sixpack.js
-mv sixpack.js constructorio-ab.js
-mv mocha/sixpack-test.js mocha/constructorio-ab-test.js
